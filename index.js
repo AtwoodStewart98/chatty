1066fa03409b8c75acf0b080956f2566d315f61a
Stewart Atwood <stewartatwood@Stewarts-MacBook-Pro.local>
2017-09-19 14:55:10 -0500

    top kek

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
 
+var messages = [];
+
+var app = express();
+
+app.use(express.static("assets"));
+app.use(bodyParser.json());
+
+app.get("/messages", function(req, res, next) {
+  res.status(200).json({ messages: messages });
+});
+
+app.post("/messages", function(req, res, next) {
+  messages.push({ message: req.body.message, time: new Date() });
+  res.status(200).json({ messages: messages });
+});
+
+app.listen(3000, function() {
+  console.log("Listening on Port: 3000");
+});
